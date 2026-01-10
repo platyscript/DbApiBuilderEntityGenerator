99f543bef54dd5c4ea56402e77c96c67df70b005
platyscript <244315239+platyscript@users.noreply.github.com>
2026-01-10 06:54:39 +0530

    Create command files (#2)
    * Added new projects
    
    * Added yaml file

---
+++ src/DbApiBuilderEntityGenerator.Core/Options/EntityNaming.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DbApiBuilderEntityGenerator.Core.Options;
+
+/// <summary>
+/// Entity class naming strategy
+/// </summary>
+public enum EntityNaming
+{
+  /// <summary>
+  /// Use table name as entity name
+  /// </summary>
+  Preserve = 0,
+
+  /// <summary>
+  /// Use table name in plural form
+  /// </summary>
+  Plural = 1,
+
+  /// <summary>
+  /// Use table name in singular form
+  /// </summary>
+  Singular = 2
+
+}
---
+++ src/DbApiBuilderEntityGenerator.Core/Serialization/GeneratorModel.cs
@@ -0,0 +1,107 @@
+using System;
+using System.ComponentModel;
+using DbApiBuilderEntityGenerator.Core.Options;
+
+namespace DbApiBuilderEntityGenerator.Core.Serialization;
+
+/// <summary>
+/// Generator Options
+/// </summary>
+public class GeneratorModel
+{
+
+  public GeneratorModel()
+  {
+    RelationshipNaming = RelationshipNaming.Plural;
+    EntityNaming = EntityNaming.Singular;
+  }
+
+  /// <summary>
+  /// Gets or sets the project directory.
+  /// </summary>
+  /// <value>
+  /// The project directory.
+  /// </value>
+  public string? OutputDirectory { get; set; }
+
+  /// <summary>
+  /// Gets or sets the database to generate code for.
+  /// </summary>
+  /// <value>
+  /// The database to generate code for.
+  /// </value>
+  [DefaultValue(DatabaseProviders.SqlServer)]
+  public DatabaseProviders Provider { get; set; } = DatabaseProviders.SqlServer;
+
+  /// <summary>
+  /// Gets or sets the connection string for reverse engineering the database
+  /// </summary>
+  /// <value>
+  /// The connection string for reverse engineering the database
+  /// </value>
+  public string? ConnectionString { get; set; }
+
+  /// <summary>
+  /// Gets or sets the name of the connection in the user secret file.
+  /// </summary>
+  /// <value>
+  /// The name of the connection.
+  /// </value>
+  public string? ConnectionName { get; set; }
+
+  /// <summary>
+  /// Gets or sets the user secrets identifier. A user secrets ID is unique value used to store and identify a collection of secret configuration values.
+  /// </summary>
+  /// <value>
+  /// The user secrets identifier.
+  /// </value>
+  public string? UserSecretsId { get; set; }
+
+  /// <summary>
+  /// Gets or sets the tables to include in the model, or an empty enumerable to include all
+  /// </summary>
+  /// <value>
+  /// The tables to include in the model, or an empty enumerable to include all
+  /// </value>
+  public List<string>? Tables { get; set; }
+
+  /// <summary>
+  /// Gets or sets the schema to include in the model, or an empty enumerable to include all.
+  /// </summary>
+  /// <value>
+  /// The schema to include in the model, or an empty enumerable to include all.
+  /// </value>
+  public List<string>? Schemas { get; set; }
+
+  /// <summary>
+  /// Gets or sets the model that specifies which elements should be ignored during processing.
+  /// </summary>
+  public DatabaseMatchModel? Exclude { get; set; }
+
+  /// <summary>
+  /// Gets or sets the entity class naming strategy.
+  /// </summary>
+  /// <value>
+  /// The entity class naming strategy.
+  /// </value>
+  [DefaultValue(EntityNaming.Singular)]
+  public EntityNaming EntityNaming { get; set; }
+
+  /// <summary>
+  /// Gets or sets the relationship property naming strategy.
+  /// </summary>
+  /// <value>
+  /// The relationship property naming strategy.
+  /// </value>
+  [DefaultValue(RelationshipNaming.Plural)]
+  public RelationshipNaming RelationshipNaming { get; set; }
+
+  /// <summary>
+  /// Gets or sets the renaming expressions.
+  /// </summary>
+  /// <value>
+  /// The renaming expressions.
+  /// </value>
+  public SelectionModel? Renaming { get; set; }
+}
+
