b7cca01c2b1c6dccd9014bb898f53330b827bb48
platyscript <244315239+platyscript@users.noreply.github.com>
2026-01-13 10:39:51 +0530

    Clean up files (#4)

--- tests/TestDatabase/install-dw.sql
+++
@@ -1,8 +0,0 @@
-USE [master]
-GO
-
-RESTORE DATABASE [AdventureWorks] 
-    FROM DISK = '/adventureworks.bak'
-        WITH MOVE 'AdventureWorksDW2022' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-        MOVE 'AdventureWorksDW2022_log' TO '/var/opt/mssql/data/AdventureWorks_log.ldf'
-GO
